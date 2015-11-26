5757b195c9b5c6ea79c5d5ae28ddcdff9ea14b52
Jaime Rodriguez <jaime.rodriguez13@imperial.ac.uk>
2015-11-26 21:12:38 +0000

    [J] Graph colouring now with fgl library's Graph type. Added tests within file

--- src/RegisterAllocation/GraphColouring.hs
+++ src/RegisterAllocation/GraphColouring.hs
@@ -1,6 +1,7 @@
 module RegisterAllocation.GraphColouring where
 import qualified Data.Map as Map
 import qualified Data.Set as Set
+import Data.Graph.Inductive
 import Data.Map (Map)
 import Data.Set (Set)
 import Data.List
@@ -8,60 +9,62 @@ import CodeGen
 import GHC.Exts
 
 
-type RIG = Map Var (Set Var)
-type Stack = [Var]
+type Stack = [Int]
 type Colour = Int
 type Colours = [Colour]
 
-colourGraph :: RIG -> Colours -> Maybe (Map Var Colour)
+--testing
+n = [1,2,3,4,5,6]
+es = [[3,6],[3,5,6],[1,2,4,5,6],[3,5,6],[2,3,4,6],[1,2,3,4,5]]
+
+ledges :: [LEdge ()]
+ledges = map (\(a,b) -> (a,b,())) $ concatMap (\(x,xs) -> zip (repeat x) xs) (zip n es)
+
+lnodes :: [LNode Int]
+lnodes = map (\a -> (a, 0)) n
+
+testg :: Gr Int ()
+testg = mkGraph lnodes ledges
+--testing
+
+colourGraph :: Graph gr => gr a () -> Colours -> Maybe (Map Node Colour)
 colourGraph rig colours
   = case stack of
     Just s -> findColouring s rig colours 
     Nothing -> Nothing
   where
-    stack = extractWhileNonempty sortedGraph [] (length colours)
-    sortedGraph = sortWith (\(_, set) -> Set.size set) (Map.toList rig)
+    stack = extractWhileNonempty rig [] (length colours)
     
+extractWhileNonempty :: Graph gr => gr a () -> Stack -> Int -> Maybe Stack
+extractWhileNonempty rig stack maxR
+  | isEmpty rig = Just stack
+  | otherwise = case findValidNode rig (nodes rig) maxR of 
+        Just n  -> extractWhileNonempty (delNode n rig) (n : stack) maxR
+        Nothing -> Nothing
+     
+findValidNode :: Graph gr => gr a () -> [Node] -> Int -> Maybe Node
+findValidNode rig (x:xs) maxR
+  | length (neighbors rig x) < maxR = Just x
+  | otherwise                       = findValidNode rig xs maxR
+findValidNode _ [] _ = Nothing
 
-extractWhileNonempty :: [(Var, Set Var)] -> Stack -> Int -> Maybe Stack
-extractWhileNonempty ((var, edges) : xs) stack maxColours
-  | Set.size edges <= maxColours = extractWhileNonempty (remove var xs) (var : stack) maxColours
-  | otherwise                    = Nothing
-extractWhileNonempty [] stack _  = Just stack
-
-remove :: Var -> [(Var, Set Var)] -> [(Var, Set Var)]
-remove x ((y, ys) : rest) 
-  | x == y = rest
-  | otherwise = (y, Set.difference ys (Set.fromList [x])) : remove x rest 
-remove _ [] = []
-
-
-
-findColouring :: [Var] -> RIG -> [Colour] -> Maybe (Map Var Colour)
-findColouring vars rig allCol
-  = foldl maybeColour (Just Map.empty) vars
+findColouring :: Graph gr => [Node] -> gr a () -> [Colour] -> Maybe (Map Node Colour)
+findColouring nodes rig allCol
+  = foldl maybeColour (Just Map.empty) nodes
   where
-    maybeColour :: Maybe (Map Var Colour) -> Var -> Maybe (Map Var Colour) 
+    maybeColour :: Maybe (Map Node Colour) -> Node -> Maybe (Map Node Colour) 
     maybeColour Nothing _ = Nothing
-    maybeColour (Just colouring) var = 
-      case differentColour var rig colouring allCol of
-        Just col -> Just $ Map.insert var col colouring
+    maybeColour (Just colouring) node = 
+      case getNewColour (neighbors rig node) allCol colouring of
+        Just col -> Just $ Map.insert node col colouring
         Nothing  -> Nothing
-
-
-differentColour :: Var -> RIG -> Map Var Colour -> [Colour] -> Maybe Colour
-differentColour v rig coloured allCol
-  = case allNeighbors of
-      Nothing -> Just (head allCol)
-      Just k  -> getNewColour (Set.elems k) allCol
-  where
-    allNeighbors = Map.lookup v rig
-    -- getnewcolour given the list of neighbors
-    getNewColour :: [Var] -> [Colour] -> Maybe Colour
-    getNewColour (n : rest) cols 
-      = case Map.lookup n coloured of
-          Nothing -> getNewColour rest cols
-          Just c -> getNewColour rest (cols \\ [c])
-    getNewColour (_:_) [] = Nothing
-    getNewColour [] cols = Just (head cols)
-
+    
+getNewColour :: [Node] -> [Colour] -> Map Node Colour -> Maybe Colour
+getNewColour (_:_) [] _
+  = Nothing
+getNewColour (n : rest) cols coloured
+  = case Map.lookup n coloured of
+      Nothing -> getNewColour rest cols coloured
+      Just c -> getNewColour rest (cols \\ [c]) coloured
+getNewColour [] cols _
+  = Just (head cols)
