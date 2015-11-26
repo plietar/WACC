4f0a298ed61d1efe81824119d2aa4f5bab31f0d0
Jaime Rodriguez <jaime.rodriguez13@imperial.ac.uk>
2015-11-26 16:53:17 +0000

    [J] Graph colouring algorithm compiling, not fully functional yet

--- src/RegisterAllocation/GraphColouring.hs
+++ src/RegisterAllocation/GraphColouring.hs
@@ -1,2 +1,89 @@
 module RegisterAllocation.GraphColouring where
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+import Data.Map (Map)
+import Data.Set (Set)
+import Data.List
+import CodeGen
+import GHC.Exts
+
+
+type RIG = Map Var (Set Var)
+type Stack = [Var]
+type Colour = Int
+
+maxRegisters :: Int
+maxRegisters = 32
+
+colours :: [Int]
+colours = [0..31]
+
+sampleRig :: RIG
+sampleRig = Map.fromList $ zip (map Var [1..6]) $ map Set.fromList $ map (map Var) lists
+  where
+    lists = [[3,6],[3,5,6],[1,2,4,5,6],[3,5,6],[2,3,4,6],[1,2,3,4,5]] 
+
+push :: a -> [a] -> Maybe ((), [a])
+push elem stack = Just ((), elem : stack)
+
+
+pop :: [a] -> Maybe (a, [a])
+pop (x : xs) = Just (x, xs)
+pop [] = Nothing
+
+colourGraph :: RIG -> Maybe (Map Var Colour)
+colourGraph rig
+  = case stack of
+    Just s -> findColouring s rig colours 
+    Nothing -> Nothing
+  where
+    stack = extractWhileNonempty sortedGraph []
+    sortedGraph = sortWith (\(_, set) -> Set.size set) (Map.toList rig)
+    vars = sortWith (\(Var x) -> x) (map fst (Map.toList rig))
+    
+
+extractWhileNonempty :: [(Var, Set Var)] -> Stack -> Maybe Stack
+extractWhileNonempty [(var, edges)] stack = Just stack
+extractWhileNonempty ((var, edges) : xs) stack
+  | Set.size edges < maxRegisters = extractWhileNonempty (remove var xs) pushedStack  
+  | otherwise                     = Nothing
+  where
+    Just (_, pushedStack) = push var stack
+extractWhileNonempty _ _ = Nothing
+
+remove :: Var -> [(Var, Set Var)] -> [(Var, Set Var)]
+remove x ((y, ys) : rest) 
+  | x == y = rest
+  | otherwise = (y, Set.difference ys (Set.fromList [x])) : remove x rest 
+remove _ [] = []
+
+
+
+findColouring :: [Var] -> RIG -> [Colour] -> Maybe (Map Var Colour)
+findColouring vars rig allCol
+  = foldl maybeColour (Just Map.empty) vars
+  where
+    maybeColour :: Maybe (Map Var Colour) -> Var -> Maybe (Map Var Colour) 
+    maybeColour Nothing _ = Nothing
+    maybeColour (Just colouring) var = 
+      case differentColour var rig colouring allCol of
+        Just col -> Just $ Map.insert var col colouring
+        Nothing  -> Nothing
+
+
+differentColour :: Var -> RIG -> Map Var Colour -> [Colour] -> Maybe Colour
+differentColour v rig coloured allCol
+  = case allNeighbors of
+      Nothing -> Just (head allCol)
+      Just k  -> getNewColour (Set.elems k) allCol
+  where
+    allNeighbors = Map.lookup v rig
+    -- getnewcolour given the list of neighbors
+    getNewColour :: [Var] ->  [Colour] -> Maybe Colour
+    getNewColour (n : rest) cols 
+      = case Map.lookup n coloured of
+          Nothing -> getNewColour rest cols
+          Just c -> getNewColour rest (cols \\ [c])
+    getNewColour vars [] = Nothing
+    getNewColours [] cols = Just (head cols)
 
