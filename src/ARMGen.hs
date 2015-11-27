5be4d2a38f57fe6b93114b030c932c78144cb701
Nacho <ignacio.navarro14@imperial.ac.uk>
2015-11-27 17:19:48 +0000

    [J][N] Changed the return type to a Writer Monad

--- src/ARMGen.hs
+++ src/ARMGen.hs
@@ -2,75 +2,107 @@ module GenARM where
 
 import AST
 import CodeGen
+import Data.Monoid
+import Control.Monad.Writer
+import Control.Monad.State
 
 
-genARMInstruction :: IR -> [String]
-genARMInstruction (ILiteral { iDest = Var dest, iLiteral = LitInt n } ) 
-  = ["LDR r" ++ (show dest) ++ ", =" ++ (show n)]
+data ARMState = ARMState
+  {
+    literals :: [Int]
+  }
+
+data ARMWriter = ARMWriter
+  { assembly :: [String] 
+  , allocated :: [(String, String)]
+  }
+
+instance Monoid ARMWriter where
+  mempty = ARMWriter [] []
+  mappend (ARMWriter a b) (ARMWriter a' b') = ARMWriter (a ++ a') (b ++ b')
+
+emit :: [String] -> WriterT ARMWriter (State ARMState) ()
+emit xs = tell (ARMWriter xs [])
+
+emitLiteral :: String -> WriterT ARMWriter (State ARMState) String 
+emitLiteral s = do
+  (l:ls) <- gets literals
+  modify (\s -> s { literals = ls })
+  return ("msg_ " ++ show l)
+
+genARMInstruction :: IR -> WriterT ARMWriter (State ARMState) ()
+genARMInstruction (ILiteral { iDest = Var dest, iLiteral = LitInt n }) 
+  = emit ["LDR r" ++ (show dest) ++ ", =" ++ (show n)]
 genARMInstruction (ILiteral { iDest = Var dest, iLiteral = LitBool True } ) 
-  = ["MOV r" ++ (show dest) ++ ", #1"]
+  = emit ["MOV r" ++ (show dest) ++ ", #1"]
 genARMInstruction (ILiteral { iDest = Var dest, iLiteral = LitBool False } ) 
-  = ["MOV r" ++ (show dest) ++ ", #0"]
+  = emit ["MOV r" ++ (show dest) ++ ", #0"]
 genARMInstruction (ILiteral { iDest = Var dest, iLiteral = LitChar chr } ) 
-  = ["MOV r" ++ (show dest) ++ ", #" ++ (show chr)]
+  = emit ["MOV r" ++ (show dest) ++ ", #" ++ (show chr)]
 -- Not right, need a reference of the number of strings in the scope.
 -- For the moment I'll just put msg_0.
-genARMInstruction (ILiteral { iDest = Var dest, iLiteral = LitString str  } ) 
-  = ["LDR r" ++ (show dest) ++ ", =msg_0"]
+genARMInstruction (ILiteral { iDest = Var dest, iLiteral = LitString str  } ) = do
+  message <- emitLiteral str 
+  emit ["LDR r" ++ (show dest) ++ ", =" ++ message]
 
 --BinOp
 genARMInstruction (IBinOp { iBinOp = op, iDest = Var dest, 
         iLeft  = Var left, iRight = Var right } )
   = case op of  
-      BinOpAdd -> ["ADD r" ++ (show dest) ++ ", r" ++ (show left) ++ ", r" ++ (show right) ] 
-      BinOpSub -> ["SUB r" ++ (show dest) ++ ", r" ++ (show left) ++ ", r" ++ (show right) ]
-      BinOpMul -> ["MUL r" ++ (show dest) ++ ", r" ++ (show left) ++ ", r" ++ (show right) ]
-      BinOpDiv -> ["MOV r0, r" ++ (show left),
-                   "MOV r1, r" ++ (show right),
-                   "BL p_check_divide_by_zero",
-                   "BL __aeabi_idiv"]
-      BinOpRem -> ["MOV r0, r" ++ (show left),
-                   "MOV r1, r" ++ (show right),
-                   "BL p_check_divide_by_zero",
-                   "BL __aeabi_idivmod"]
-      BinOpGT  -> ["CMP r" ++ (show left) ++ ", r" ++ (show right),
-                 "MOVGT r" ++ (show dest) ++ ", #1",
-                 "MOVLE r" ++ (show dest) ++ ", #0"]
-      BinOpGE  ->  ["CMP r" ++ (show left) ++ ", r" ++ (show right),
-                 "MOVGE r" ++ (show dest) ++ ", #1",
-                 "MOVLT r" ++ (show dest) ++ ", #0"]
-      BinOpLT  ->  ["CMP r" ++ (show left) ++ ", r" ++ (show right),
-                 "MOVLT r" ++ (show dest) ++ ", #1",
-                 "MOVGE r" ++ (show dest) ++ ", #0"]
-      BinOpLE  ->  ["CMP r" ++ (show left) ++ ", r" ++ (show right),
-                 "MOVLE r" ++ (show dest) ++ ", #1",
-                 "MOVGT r" ++ (show dest) ++ ", #0"]
-      BinOpEQ  ->  ["CMP r" ++ (show left) ++ ", r" ++ (show right),
-                 "MOVEQ r" ++ (show dest) ++ ", #1",
-                 "MOVNE r" ++ (show dest) ++ ", #0"]
-      BinOpNE  ->  ["CMP r" ++ (show left) ++ ", r" ++ (show right),
-                 "MOVNE r" ++ (show dest) ++ ", #1",
-                 "MOVEQ r" ++ (show dest) ++ ", #0"]
-      BinOpAnd ->  ["AND r" ++ (show dest) ++ ", r" ++ (show left) ++ ", r" ++ (show right) ]
-      BinOpOr  ->  ["OR r" ++ (show dest) ++ ", r" ++ (show left) ++ ", r" ++ (show right) ] 
+      BinOpAdd -> emit ["ADD r" ++ (show dest) ++ ", r" ++ 
+                        (show left) ++ ", r" ++ (show right) ] 
+      BinOpSub -> emit ["SUB r" ++ (show dest) ++ ", r" ++ 
+                        (show left) ++ ", r" ++ (show right) ]
+      BinOpMul -> emit ["MUL r" ++ (show dest) ++ ", r" ++ 
+                        (show left) ++ ", r" ++ (show right) ]
+      BinOpDiv -> emit ["MOV r0, r" ++ (show left),
+                        "MOV r1, r" ++ (show right),
+                        "BL p_check_divide_by_zero",
+                        "BL __aeabi_idiv"]
+      BinOpRem -> emit ["MOV r0, r" ++ (show left),
+                        "MOV r1, r" ++ (show right),
+                        "BL p_check_divide_by_zero",
+                        "BL __aeabi_idivmod"]
+      BinOpGT  -> emit ["CMP r" ++ (show left) ++ ", r" ++ (show right),
+                        "MOVGT r" ++ (show dest) ++ ", #1",
+                        "MOVLE r" ++ (show dest) ++ ", #0"]
+      BinOpGE  -> emit ["CMP r" ++ (show left) ++ ", r" ++ (show right),
+                       "MOVGE r" ++ (show dest) ++ ", #1",
+                       "MOVLT r" ++ (show dest) ++ ", #0"]
+      BinOpLT  -> emit ["CMP r" ++ (show left) ++ ", r" ++ (show right),
+                        "MOVLT r" ++ (show dest) ++ ", #1",
+                        "MOVGE r" ++ (show dest) ++ ", #0"]
+      BinOpLE  -> emit ["CMP r" ++ (show left) ++ ", r" ++ (show right),
+                        "MOVLE r" ++ (show dest) ++ ", #1",
+                        "MOVGT r" ++ (show dest) ++ ", #0"]
+      BinOpEQ  -> emit ["CMP r" ++ (show left) ++ ", r" ++ (show right),
+                        "MOVEQ r" ++ (show dest) ++ ", #1",
+                        "MOVNE r" ++ (show dest) ++ ", #0"]
+      BinOpNE  -> emit ["CMP r" ++ (show left) ++ ", r" ++ (show right),
+                        "MOVNE r" ++ (show dest) ++ ", #1",
+                        "MOVEQ r" ++ (show dest) ++ ", #0"]
+      BinOpAnd -> emit ["AND r" ++ (show dest) ++ ", r" ++ 
+                        (show left) ++ ", r" ++ (show right) ]
+      BinOpOr  -> emit ["OR r" ++ (show dest) ++ ", r" ++ 
+                        (show left) ++ ", r" ++ (show right) ] 
 
 --UnOp
-genARMInstruction (IUnOp { iUnOp = op, iDest = Var dest, 
-        iValue = Var value } )
+genARMInstruction IUnOp { iUnOp = op, iDest = Var dest, 
+        iValue = Var value } 
   = case op of
-      UnOpNot -> ["EOR r" ++ (show dest) ++ ", r" ++ (show value) ++ ", #1"]
-      UnOpNeg -> ["RSBS r" ++ (show dest) ++ ", r" ++ (show value) ++ ", #0"]
-      UnOpLen -> ["LDR r" ++ (show dest) ++ ", =msg_0",
-                  "LDR r" ++ (show value) ++ ", =msg_0"]
-      UnOpOrd -> [""]
-      UnOpChr -> [""]
+      UnOpNot -> emit ["EOR r" ++ (show dest) ++ ", r" ++ (show value) ++ ", #1"]
+      UnOpNeg -> emit ["RSBS r" ++ (show dest) ++ ", r" ++ (show value) ++ ", #0"]
+      UnOpLen -> emit ["LDR r" ++ (show dest) ++ ", =msg_0",
+                       "LDR r" ++ (show value) ++ ", =msg_0"]
+      UnOpOrd -> emit [""]
+      UnOpChr -> emit [""]
 
 --Jumps
 genARMInstruction (ICondJump { iLabel = label, iValue = value}) 
-  = ["CMP r" ++ (show value) ++ ", #0",
-     "BNE" ++ (show label)]
+  = emit ["CMP r" ++ (show value) ++ ", #0",
+          "BNE" ++ (show label)]
 genARMInstruction (IJump {iLabel = label} ) 
-  = ["B " ++ (show label)]
+  = emit ["B " ++ (show label)]
 
 --Call
 genARMInstruction (ICall { iLabel = label, iArgs = vars, iDest = dest })
@@ -78,19 +110,19 @@ genARMInstruction (ICall { iLabel = label, iArgs = vars, iDest = dest })
 
 --Labels
 genARMInstruction (ILabel { iLabel = NamedLabel label} )
-  = [label ++ ":"] 
+  = emit [label ++ ":"] 
 genARMInstruction (ILabel { iLabel = UnnamedLabel n }) 
-  = ["L" ++ (show n) ++ ":"]
+  = emit ["L" ++ (show n) ++ ":"]
 
 --Frame
 genARMInstruction (IFrameAllocate { iSize = size })
-  = ["SUB sp, sp, #" ++ (show size)]
+  = emit ["SUB sp, sp, #" ++ (show size)]
 genARMInstruction (IFrameFree { iSize = size } )
-  = ["ADD sp, sp, #" ++ (show size)]
+  = emit ["ADD sp, sp, #" ++ (show size)]
 genARMInstruction (IFrameRead {iOffset = offset, iDest = Var dest} ) 
-  = ["LDR r" ++ (show dest) ++ ", [sp, #" ++ (show offset) ++ "]"]
+  = emit ["LDR r" ++ (show dest) ++ ", [sp, #" ++ (show offset) ++ "]"]
 genARMInstruction (IFrameWrite {iOffset = offset, iValue = Var value} )
-  = ["STR r" ++ (show value) ++ ", [sp, #" ++ (show offset) ++ "]"]
+  = emit ["STR r" ++ (show value) ++ ", [sp, #" ++ (show offset) ++ "]"]
 
  
 
