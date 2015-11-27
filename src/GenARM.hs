cfdb2ab08c501bf4fcab8d7a3db7bab08fd318f0
Nacho <ignacio.navarro14@imperial.ac.uk>
2015-11-27 12:32:43 +0000

    [N] Haven't completed div and mod, the reference compiler calls an external label. Starting on comparison operators

--- src/GenARM.hs
+++ src/GenARM.hs
@@ -4,25 +4,34 @@ import AST
 import CodeGen
 
 
-genARMInstruction :: IR -> String
+genARMInstruction :: IR -> [String]
 genARMInstruction (ILiteral { iDest = Var dest, iLiteral = LitInt n } ) 
-  = "LDR " ++ "r" ++ (show dest) ++ ", =" ++ (show n)
+  = ["LDR r" ++ (show dest) ++ ", =" ++ (show n)]
 genARMInstruction (ILiteral { iDest = Var dest, iLiteral = LitBool True } ) 
-  = "MOV " ++ "r" ++ (show dest) ++ ", #" ++ (show 1)
+  = ["MOV r" ++ (show dest) ++ ", #" ++ (show 1)]
 genARMInstruction (ILiteral { iDest = Var dest, iLiteral = LitBool False } ) 
-  = "MOV " ++ "r" ++ (show dest) ++ ", #" ++ (show 0)
+  = ["MOV r" ++ (show dest) ++ ", #" ++ (show 0)]
 genARMInstruction (ILiteral { iDest = Var dest, iLiteral = LitChar chr } ) 
-  = "MOV " ++ "r" ++ (show dest) ++ ", #" ++ (show chr)
+  = ["MOV r" ++ (show dest) ++ ", #" ++ (show chr)]
 -- Need a reference of the number of strings in the scope.
 genARMInstruction (ILiteral { iDest = Var dest, iLiteral = LitString str  } ) 
-  = "LDR " ++ "r" ++ (show dest) ++ ", =msg_" ++ (show 0)
+  = ["LDR r" ++ (show dest) ++ ", =msg_" ++ (show 0)]
+
+--BinOp
+--Missing divide and mod binops, ARM calls a label "__aeabi_idiv" 
+--but doesn't show the label.
+genARMInstruction (IBinOp { iBinOp = op, iDest = Var dest, 
+        iLeft  = Var left, iRight = Var right } )
+  = case op of  
+    BinOpAdd -> ["ADD r" ++ (show dest) ++ ", r" ++ (show left) ++ ", r" ++ (show right) ] 
+    BinOpSub -> ["SUB r" ++ (show dest) ++ ", r" ++ (show left) ++ ", r" ++ (show right) ]
+    BinOpMul -> ["MUL r" ++ (show dest) ++ ", r" ++ (show left) ++ ", r" ++ (show right) ]
+    BinOpDiv -> [""]
+    BinOpRem -> [""]
+    BinOpGT  -> ["CMP r" ++ (show left) ++ ", r" ++ (show right),
+                 "MOVGT r" ++ (show left) ++ ", #" ++ (show 1),
+                 "MOVLE r" ++ (show left) ++ ", #" ++ (show 0)]
 
 
 
 
-genARMInstruction (IBinOp { iBinOp = op, iDest = dest, 
-        iLeft  = left, iRight = right } )
-  	= case op of  
-    BinOpAdd -> "" 
-    BinOpSub -> ""
-    BinOpMul -> ""
