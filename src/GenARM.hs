08b9abb863e2fa88bc754865a2cd0e42d72f91c3
Nacho <ignacio.navarro14@imperial.ac.uk>
2015-11-27 11:48:12 +0000

    [N] Literals now with their corresponding ARM instruction.

--- src/GenARM.hs
+++ src/GenARM.hs
@@ -5,9 +5,24 @@ import CodeGen
 
 
 genARMInstruction :: IR -> String
-genARMInstruction (ILiteral { iDest = Var dest, iLiteral = Var lit } ) 
-  = "LDR " ++ "r" ++ (show dest) ++ ", =" ++ (show lit)
+genARMInstruction (ILiteral { iDest = Var dest, iLiteral = LitInt n } ) 
+  = "LDR " ++ "r" ++ (show dest) ++ ", =" ++ (show n)
+genARMInstruction (ILiteral { iDest = Var dest, iLiteral = LitBool True } ) 
+  = "MOV " ++ "r" ++ (show dest) ++ ", #" ++ (show 1)
+genARMInstruction (ILiteral { iDest = Var dest, iLiteral = LitBool False } ) 
+  = "MOV " ++ "r" ++ (show dest) ++ ", #" ++ (show 0)
+genARMInstruction (ILiteral { iDest = Var dest, iLiteral = LitChar chr } ) 
+  = "MOV " ++ "r" ++ (show dest) ++ ", #" ++ (show chr)
+-- Need a reference of the number of strings in the scope.
+genARMInstruction (ILiteral { iDest = Var dest, iLiteral = LitString str  } ) 
+  = "LDR " ++ "r" ++ (show dest) ++ ", =msg_" ++ (show 0)
 
---genARMInstruction (IBinOp { iBinOp = op, iDest = dest, 
---                            iLeft  = left, iRight = right } )
---  = 
+
+
+
+genARMInstruction (IBinOp { iBinOp = op, iDest = dest, 
+        iLeft  = left, iRight = right } )
+  	= case op of  
+    BinOpAdd -> "" 
+    BinOpSub -> ""
+    BinOpMul -> ""
